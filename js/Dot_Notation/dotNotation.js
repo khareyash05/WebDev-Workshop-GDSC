a33749bc45a3e921b2d10fa623add5ecea97c00c
abhinavv9 <68147871+abhinavv9@users.noreply.github.com>
2022-01-06 23:57:06 +0530

    Added in js

---
+++ js/Dot_Notation/dotNotation.js
@@ -0,0 +1,13 @@
+let obj = {
+    cat: 'meow',
+    dog: 'woof'
+  };
+
+  let soundCat = obj.cat;
+  let soundDog = obj.dog;
+
+  console.log(soundCat);
+  // meow
+
+  console.log(soundDog);
+  //Woof
